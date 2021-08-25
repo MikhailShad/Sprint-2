0f5110b688165a8d06da40e6da690b5e0c17e7b2
Шадрин Михаил Дмитриевич <shadrin.m.dm@sberbank.ru>
2021-08-25 13:50:28 +0300

    simplified ProblemB solution

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -7,16 +7,8 @@ class ProblemB {
             val targetStringLength: Int = params[0]
             val charactersNumber: Int = params[1]
 
-            val minimalOccurrences = targetStringLength / charactersNumber
-
             var targetString = ""
-            // Добавляем минимальное допустимое количество символов в строку
-            for (i in 0 until charactersNumber) {
-                targetString += "${'a'.plus(i % charactersNumber)}".repeat(minimalOccurrences)
-            }
-
-            // Добиваем строку до нужной длины символами по кругу
-            for (i in targetString.length until targetStringLength) {
+            for (i in 0 until targetStringLength) {
                 targetString += 'a'.plus(i % charactersNumber);
             }
 
